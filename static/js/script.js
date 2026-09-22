30fb6a2549e41d3e5aa33066c46992a55ef6bd7c
Iris014 <irisccallomamani@liceovvh.cl>
2026-09-22 11:19:09 -0300

    saved

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,6 @@ console.log("Conexión exitosa con JavaScript");
 const btnMenu = document.getElementById('btn-menu');
     const menuList = document.getElementById('lista-enl');
     const menuLinks = document.querySelectorAll('.menu-item-link');
-
     const toggleMenu = () => {
         const isExpanded = btnMenu.getAttribute('aria-expanded') === 'true';
         btnMenu.setAttribute('aria-expanded', !isExpanded);
@@ -19,15 +18,8 @@ const btnMenu = document.getElementById('btn-menu');
         });
     });
 
-
 const themeToggleBtn = document.getElementById('theme-toggle');
 const body = document.body;
-
 themeToggleBtn.addEventListener('click', () => {
     body.classList.toggle('light-theme');
-    if (body.classList.contains('light-theme')) {
-        themeToggleBtn.textContent = 'DARK';
-    } else {
-        themeToggleBtn.textContent = 'LIGHT';
-    }
 });
