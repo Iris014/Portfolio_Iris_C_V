dc7a383cc66492b11b20b832017f7d0638c9cb4f
Iris014 <irisccallomamani@liceovvh.cl>
2026-09-22 15:19:18 -0300

    saved

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,20 @@
 console.log("Conexión exitosa con JavaScript");
 
 const btnMenu = document.getElementById('btn-menu');
-    const menuList = document.getElementById('lista-enl');
-    const menuLinks = document.querySelectorAll('.menu-item-link');
-    const toggleMenu = () => {
-        const isExpanded = btnMenu.getAttribute('aria-expanded') === 'true';
-        btnMenu.setAttribute('aria-expanded', !isExpanded);
-        menuList.classList.toggle('is-active');
-        btnMenu.classList.toggle('is-active');
-    };
-    btnMenu.addEventListener('click', toggleMenu);
-    menuLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            if (menuList.classList.contains('is-active')) {
-                toggleMenu();
-            }
-        });
+const menuList = document.getElementById('lista-enl');
+const menuLinks = document.querySelectorAll('.menu-item-link');
+const toggleMenu = () => {
+    menuList.classList.toggle('is-active');
+    btnMenu.classList.toggle('is-active');
+};
+btnMenu.addEventListener('click', toggleMenu);
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (menuList.classList.contains('is-active')) {
+            toggleMenu();
+        }
     });
+});
 
 const themeToggleBtn = document.getElementById('theme-toggle');
 const body = document.body;
